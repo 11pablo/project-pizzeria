3f4902563d4e597e39c5984e07f8c7585e311318
Pawel Wieczorek <pawel1wiecz@gmail.com>
2021-12-17 19:45:28 +0100

    Add accordion

--- src/js/script.js
+++ src/js/script.js
@@ -52,14 +52,15 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
-  class Product{  //deklaracja klasy - konstruktor
+  class Product{  //deklaracja klasy, konstruktor
     constructor(id, data){
-      const thisProduct = this; //na metodę app
+      const thisProduct = this; 
 
       thisProduct.id = id;
       thisProduct.data = data;
 
-      thisProduct.renderInMenu(); // uruchomi funkcję po utworzeniu instancji
+      thisProduct.renderInMenu(); 
+      thisProduct.initAccordion();
 
       console.log('new produkt:',thisProduct);
     }
@@ -74,7 +75,28 @@
       /*find menu continer*/
       const menuContainer = document.querySelector(select.containerOf.menu);
       /*add element to menu*/
-      menuContainer.appendChild(thisProduct.element);
+      menuContainer.appendChild(thisProduct.element); 
+    }
+
+    initAccordion(){
+      const thisProduct = this;
+      /*console.log(' thisProduct:', thisProduct.element);*/
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable); 
+      console.log('clickableTrigger:', clickableTrigger);
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault(); // wyłącz domyślną akcję do zdarzenia
+        /* find active product (product that has active class) */ 
+        const activeProduct = document.querySelector(select.all.menuProductsActive); 
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if(activeProduct != null && activeProduct != thisProduct.element) { 
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive); 
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
     }
   }
 
@@ -83,17 +105,17 @@
       const thisApp = this;
       console.log('thisApp.data:', thisApp.data);
       for(let productData in thisApp.data.products){ 
-        new Product(productData, thisApp.data.products[productData]); //tworzenie nowej instancji dla karzdego produktu, przkazujemy nazwę włąściwości(produktu) ale i obiekt kktury się pod nią kryje
+        new Product(productData, thisApp.data.products[productData]); 
       }
       
       /*const testProduct = new Product();
       console.log('testProduct:', testProduct);*/
     },
 
-    initData: function(){ //metoda przygotowywuje dostęp do danych z obiektu dataSource
+    initData: function(){ 
       const thisApp = this;
       console.log(thisApp);
-      thisApp.data = dataSource; //przez referencje
+      thisApp.data = dataSource; 
       
 
       
