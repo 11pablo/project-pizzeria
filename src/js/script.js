b6c66c5473c8104b106ea303963ab2665875a0e5
Pawel Wieczorek <pawel1wiecz@gmail.com>
2021-12-19 11:05:56 +0100

    Add calculate prices

--- src/js/script.js
+++ src/js/script.js
@@ -59,10 +59,13 @@
       thisProduct.id = id;
       thisProduct.data = data;
 
-      thisProduct.renderInMenu(); 
+      thisProduct.renderInMenu(); //wywołąnia
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
 
-      console.log('new produkt:',thisProduct);
+      /*console.log('new produkt:',thisProduct);*/
     }
 
     renderInMenu(){ //metoda renderInMenu
@@ -78,14 +81,26 @@
       menuContainer.appendChild(thisProduct.element); 
     }
 
+    getElements(){  /*dostęp do elementów*/
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      /*console.log('getElements:', thisProduct.priceEle);*/
+    }
+
     initAccordion(){
       const thisProduct = this;
       /*console.log(' thisProduct:', thisProduct.element);*/
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable); 
-      console.log('clickableTrigger:', clickableTrigger);
+      /*const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);*/
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      /*console.log('clickableTrigger:', thisProduct.accordionTrigger);*/
       /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function(event) {
+      thisProduct.accordionTrigger.addEventListener('click', function(event) {
         /* prevent default action for event */
         event.preventDefault(); // wyłącz domyślną akcję do zdarzenia
         /* find active product (product that has active class) */ 
@@ -98,12 +113,76 @@
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
+
+    initOrderForm(){ 
+      const thisProduct = this;
+      /*console.log('initOrderFrom:');*/
+
+      thisProduct.form.addEventListener('submit', function(event){ //nasłuchiwanie submita
+        event.preventDefault(); //blokowanie  domyślnych akcji
+        thisProduct.processOrder();
+      });
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder(){
+      const thisProduct = this;
+      const formData = utils.serializeFormToObject(thisProduct.form); 
+      /*console.log('formData', formData);*/
+
+      // set price to default price
+      let price = thisProduct.data.price; //tworzymy zmienną w której będziemy przechowywać cenę
+
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) { //przejście po kategoriach
+      // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId]; 
+        //console.log('paramId:',paramId); //paramId np coffee sauce topping crust
+        /*console.log('param:',param);*/
+
+        // for every option in this category
+        for(let optionId in param.options) {  //przejście po opcjach
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(option); //options np.red pepers
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+            // check if the option is not default
+            if(option.default !== true) {
+              // add option price to price variable
+              price += option.price;
+            }
+          } else {
+            // check if the option is default
+            if(option.default == true) {
+            // reduce price variable
+              price -= option.price;
+            }
+          }
+        }
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
+    }
+
   }
+
 
   const app = {
     initMenu: function(){ //deklaracja metody app init menu
       const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
+      /*console.log('thisApp.data:', thisApp.data);*/
       for(let productData in thisApp.data.products){ 
         new Product(productData, thisApp.data.products[productData]); 
       }
@@ -114,7 +193,7 @@
 
     initData: function(){ 
       const thisApp = this;
-      console.log(thisApp);
+      /*console.log(thisApp);*/
       thisApp.data = dataSource; 
       
 
@@ -123,11 +202,11 @@
 
     init: function(){
       const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
+      //console.log('*** App starting ***');
+      //console.log('thisApp:', thisApp);
+      //console.log('classNames:', classNames);
       console.log('settings:', settings);
-      console.log('templates:', templates);
+      //console.log('templates:', templates);
 
       thisApp.initData();
       thisApp.initMenu();
