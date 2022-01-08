2938845c15d663761eb9c51c3eff4e92b583b4c9
Pawel Wieczorek <pawel1wiecz@gmail.com>
2022-01-08 23:29:53 +0100

    problem 2

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,63 @@
-import {settings,select} from './settings.js'; //{} - aby wydobyć obiekty z pliku, kiedy importujemy kilka elementów i nie są domyślne
+import {settings,select, /*classNames*/} from './settings.js'; //{} - aby wydobyć obiekty z pliku, kiedy importujemy kilka elementów i nie są domyślne
 import Product from './components/Product.js'; //tyklo klasa obiekt funkcja mogą być importowane bez nawiasów
 import Cart from './components/Cart.js';
 
 const app = {
+  /*initPages: function(){
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;// odnalezienie kontenera wszystkich stron, dzięki children odnalezione wszystkie dzieci(order Booking)
+    thisApp.navLinks = document.querySelectorAll(select.nav.links); //odnalezienie wszystkich linków
+    
+    const idFromHash = window.location.hash.replace('#/',''); //wydobycie hash i podmiana #/
+    //console.log('idFromHash',idFromHash);
+    
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    //console.log('pageMatchingHash',pageMatchingHash);
+    thisApp.activatePage(pageMatchingHash); //aktywacja strony domyślnej dzięki metodzie, pierwszą znalezioną w thisApp.pages
+    //console.log('thisApp.pages:', thisApp.pages[0].id);
+
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        //get page id from href attribute 
+        const id = clickedElement.getAttribute('href').replace('#',''); //wydobycie id z atrybutu href klikniętego elementu i podmiana # na pusty ciąg znaków
+        //console.log(clickedElement,id);
+        //run thisApp.activatePage with that id 
+        thisApp.activatePage(id);
+
+        //change URL hash 
+        window.location.hash  = '#/' + id; //zmiana hash /- po to aby nie odnajdywał podobnyh id i nie przewijał strony
+      });
+    }
+  },
+
+  activatePage:function(pageId){
+    const thisApp = this;
+    //add class "active" to matching pages, remove from non-matching 
+    for(let page of thisApp.pages){ // przejście po kontenerach stron
+      page.classList.toggle(classNames.pages.active, page.id == pageId); //dodaje i usuwa klasę active w zalerzności od warunku page.id == pageId
+    }
+    //add class "active" to matching links, remove from non-matching 
+    for(let link of thisApp.navLinks){ // przejście po kontenerach stron
+      link.classList.toggle(  //dodaje i usuwa klasę (zdefiniowaną w className.nav.active) active w zalerzności od warunku 
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId //warunek
+      ); 
+      //console.log(link.classList);
+    }
+
+  },*/
+
   initMenu: function(){ 
     const thisApp = this;
     /*console.log('thisApp.data:', thisApp.data);*/
@@ -38,6 +93,8 @@ const app = {
     //console.log('classNames:', classNames);
     console.log('settings:', settings);
     //console.log('templates:', templates);
+
+    //thisApp.initPages();
 
     thisApp.initData();
     
